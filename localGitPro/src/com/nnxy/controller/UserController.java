84ac6631950677dea49ed0ee5628970162604164
wangrihua666 <117267594+wangrihua666@users.noreply.github.com>
2022-11-17 20:46:32 +0800

    Update UserController.java

--- localGitPro/src/com/nnxy/controller/UserController.java
+++ localGitPro/src/com/nnxy/controller/UserController.java
@@ -1,5 +1,11 @@
 package com.nnxy.controller;
 
   class UserController {
+    private int id;//ID
+    private String username;//用户名
+    private String password;//密码
+    private int sex;//性别
+    private String love;//爱好
+    
 
 }
